586e57bad20764591b2d61e076b16ed6d1fd19b4
Mati Reyna <matiireyna@hotmail.com>
2025-06-01 19:37:51 -0300

    Inicio de mi primer aplicacion

---
+++ client/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>WELCOME TO EXPO!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EDE9E3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
